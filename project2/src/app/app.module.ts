a4998fc0ef5352024efbb0484ae1d7668066ef16
Shrutika-Mapari <106251711+Shrutika-Mapari@users.noreply.github.com>
2022-07-11 14:59:57 +0530

    Add files via upload

---
+++ project2/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { WebsitedesignComponent } from './websitedesign/websitedesign.component';
+import { WebdevelopmentComponent } from './webdevelopment/webdevelopment.component';
+import { MobileappComponent } from './mobileapp/mobileapp.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    ServiceComponent,
+    WebsitedesignComponent,
+    WebdevelopmentComponent,
+    MobileappComponent,
+    ContactusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
